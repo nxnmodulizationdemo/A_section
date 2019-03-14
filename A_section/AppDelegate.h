dd771e58940af5a44287e03f70abe8317116c7ba
牛吉 <niuji@iyiou.com>
2019-03-14 13:51:45 +0800

    no message

---
+++ A_section/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A_section
+//
+//  Created by cocoa_niu on 2019/3/14.
+//  Copyright © 2019 cocoa_niu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
